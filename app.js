f8cfa5bcc3557040450df118e0d7392dac494531
Alesvoy <alesvoy@gmail.com>
2022-04-28 17:56:10 -0500

    Make site functional with JS

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log('first');
+// We create an IIFE to prevent using global variables in order to avoid memory leaks
+(function () {
+  const liElements = document.querySelector('ol').children;
+  const btnElement = document.querySelector('.btn');
+  const cardStarter = document.querySelector('.card--starter');
+  const cardThankYou = document.querySelector('.card--thankYou');
+  const spanEl = document.querySelector('.variable');
+
+  let clickedElement = undefined;
+
+  for (let i = 0; i < liElements.length; i++) {
+    liElements[i].addEventListener('click', () => {
+      for (let j = 0; j < liElements.length; j++) {
+        if (
+          liElements[j].hasAttribute('id') &&
+          liElements[i] !== liElements[j]
+        ) {
+          liElements[j].removeAttribute('id');
+          clickedElement = undefined;
+        }
+      }
+
+      const listItem = liElements[i];
+
+      if (listItem.hasAttribute('id')) {
+        listItem.removeAttribute('id');
+        clickedElement = undefined;
+      } else {
+        listItem.setAttribute('id', 'clicked');
+        clickedElement = listItem.textContent;
+      }
+    });
+  }
+
+  btnElement.addEventListener('click', () => {
+    cardStarter.classList.toggle('hide');
+    cardThankYou.classList.toggle('hide');
+
+    spanEl.textContent = clickedElement;
+  });
+})();
